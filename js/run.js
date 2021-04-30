08d62e03512cf222eccdc41d082cf8a24e453e1e
AdhamOmari <adhamomarics@gmail.com>
2021-05-01 02:10:37 +0300

    update js

--- js/run.js
+++ js/run.js
@@ -1,6 +1,6 @@
-
+"use strict" ;
 //alert("please answer the question !");
-
+let cunter = 0 ;
 let username = prompt("Enter you'r name ").toUpperCase();
 
 alert(username + " welcome ");
@@ -13,11 +13,13 @@ switch (myname) {
     case 'y':
         alert(myname + ' yes for sure ');
         //console.log("nice one");
+        cunter++ ;
         break;
 
     case 'yes':
         alert(myname + ' yes for sure ');
         //  console.log("not right ");
+        cunter ++
         break;
 
     case 'n':
@@ -44,10 +46,14 @@ switch (startday) {
     case 'y':
         alert('correct answer')
         //console.log('thank you')
+        cunter++ ;
+
         break;
     case 'yes':
         alert('correct answer')
         //console.log('thank you')
+        cunter++ ;
+
         break;
     case 'no':
         alert("fine");
@@ -68,6 +74,9 @@ let lovetechnolgy = prompt('do i like technology use (y,yes,no,n)').toLowerCase(
 
 if (lovetechnolgy === 'y' || lovetechnolgy === 'yes') {
     alert(lovetechnolgy + '  correct answer')
+    cunter++ ;
+    break ;
+
     //console.log('great choice ')
 
 } else if (lovetechnolgy === 'no ' || lovetechnolgy === 'n') {
@@ -81,9 +90,10 @@ if (lovetechnolgy === 'y' || lovetechnolgy === 'yes') {
 let learncode = prompt('do i like  to learn code use (y,yes,no,n)').toUpperCase();
 
 if (learncode === 'Y' || learncode === 'YES') {
+    cunter++ ;
 
     alert(learncode + ' correct answer ')
-
+break ;
 
 } else if (learncode === 'N' || learncode === 'NO') {
     alert("fine");
@@ -99,7 +109,8 @@ let lenovolap = prompt('do i use lenovo laptop use (y,n,yes.no)').toLowerCase();
 if (lenovolap == 'y' || lenovolap == 'yes' && lenovolap !== 'n' || lenovolap !== 'no') {
     alert(lenovolap + ' correct answer')
     //console.log(lenovolap);
-
+    cunter++ ;
+break ;
 
 }
 else {
@@ -112,6 +123,7 @@ else {
 let myold = prompt(' did I complete  25 years?  \' use (y,yes,no,n)');
 
 if (myold == 'y' || myold == 'yes' && myold !== 'n' || myold !== 'no') {
+    cunter++ ;
 
     alert('correct answer')
     //console.log(usegit)
@@ -128,6 +140,7 @@ else {
 let help = prompt('do i need any help  in CSS \'use just (y,yes,no,n)').toUpperCase();
 switch (help) {
     case 'Y':
+        cunter++ ;
 
         alert('correct answer');
 
@@ -140,6 +153,8 @@ switch (help) {
         break;
 
     case 'YES':
+        cunter++ ;
+
         alert('correct answer');
         //console.log("nice one");
         break;
@@ -154,21 +169,28 @@ switch (help) {
         break;
 }
 
-let myNumber = ['10', '5', '20', '14'];
+let myNumber = 5;
 
 for (let enterNumber = 1; enterNumber <= 4; enterNumber++) {
     let numper = parseInt(prompt('pleas guess my number ?'));
+
+    //for (let index = 0; index < myNumber.length; index++) {
     if (myNumber === numper) {
         alert('correct ');
+        cunter++ ;
+
+        break;
     }
 
     else if (numper > myNumber) {
         alert('too high')
-    } else if (numper < myNumber) {
+    }
+    else if (numper < myNumber) {
         alert('too low')
     }
 }
-alert('the correct answar is 10')
+
+//alert('the correct answar is 10')
 
 
 
@@ -177,21 +199,27 @@ let colors = ['red', 'black', 'green'];
 
 let flag = false;
 
-for (let attempt = 1; attempt <= 6; attempt++) {
+for (let attempt = 1; attempt <= 2; attempt++) {
 
     let useranswer = prompt("enter my  color");
 
-    for (let i = 0; i < colors.length ; i++) {
+    for (let i = 0; i < colors.length; i++) {
         if (useranswer === colors[i]) {
-            alert(useranswer +' correct');
+            alert(useranswer + ' correct');
             flag = true;
+            cunter++ ;
+
             break;
         }
+        }
+         if(flag === true) {
+            alert(useranswer + "not correct")
+            break;
     }
 
-    if (flag === true) {
-      break ;
-    }
+    
 }
+
+
 //alert(username + ' thank you for your time ');
 //console.log(username);
